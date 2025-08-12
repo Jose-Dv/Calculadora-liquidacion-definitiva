b7883976f07c030fa7eabe4117af9fa7b8765233
isasiti <isasiti06@gmail.com>
2025-08-12 11:20:27 -0500

    nombres arreglados

--- test.py
+++ test.py
@@ -1,45 +1,51 @@
 import unittest
 import liquidacion
+
 class TestLiquidacion(unittest.TestCase):
+
     def test_calcular_liquidacion_definitiva(self):
         resultado = liquidacion.calcular_liquidacion_definitiva(2000000, 365, 102854)
-        self.assertAlmostEqual(resultado["cesantias"], 2120854.0)
-        self.assertAlmostEqual(resultado["intereses_cesantias"], 252343)
-        self.assertAlmostEqual(resultado["prima"], 2102854)
-        self.assertAlmostEqual(resultado["vacaciones"], 1000000)
-        self.assertAlmostEqual(resultado["total"], 5458050)
-def test_sin_auxilio_transporte(self):
+        self.assertAlmostEqual(resultado["cesantias"], 2102854.0, 2)
+        self.assertAlmostEqual(resultado["intereses_cesantias"], 252342.48, 2)
+        self.assertAlmostEqual(resultado["prima"], 2102854.0, 2)
+        self.assertAlmostEqual(resultado["vacaciones"], 833333.33, 2)
+        self.assertAlmostEqual(resultado["total"], 5291383.81, 2)
+
+    def test_sin_auxilio_transporte(self):
         resultado = liquidacion.calcular_liquidacion_definitiva(2000000, 365, 0)
-        self.assertAlmostEqual(resultado["cesantias"], 2000000.0)
-        self.assertAlmostEqual(resultado["intereses_cesantias"], 240000.0)
-        self.assertAlmostEqual(resultado["prima"], 2000000.0)
-        self.assertAlmostEqual(resultado["vacaciones"], 2000000.0)
-        self.assertAlmostEqual(resultado["total"], 6440000.0)
-def trabajo_medio_tiempo(self):
-        resultado = liquidacion.calcular_liquidacion_definitiva(1000000,180,102854)
-        self.assertAlmostEqual(resultado["cesantias"], 1051427.0)
-        self.assertAlmostEqual(resultado["intereses_cesantias"], 126171.0)
-        self.assertAlmostEqual(resultado["prima"], 1051427.0)
-        self.assertAlmostEqual(resultado["vacaciones"], 500000.0)
-        self.assertAlmostEqual(resultado["total"], 2720025.0)
-#En caso de renuncia voluntaria, se debe calcular la liquidación definitiva igualmente, aplica todas las prestaciones sociales.
-#Caso de prueba normal
-def renuncia_voluntaria(self):
-      resultado = liquidacion.calcular_liquidacion_definitiva(1500000, 90, 102854)
-      self.assertAlmostEqual(resultado["cesantias"], 400713.50, 3)
-      self.assertAlmostEqual(resultado["intereses_cesantias"], 12021.41,3)
-      self.assertAlmostEqual(resultado["prima"], 400713.50, 3)
-      self.assertAlmostEqual(resultado["vacaciones"], 675000,3)
-      self.assertAlmostEqual(resultado["total"], 875958.41,3)
-def salario_integral(self):
+        self.assertAlmostEqual(resultado["cesantias"], 2000000.0, 2)
+        self.assertAlmostEqual(resultado["intereses_cesantias"], 240000.0, 2)
+        self.assertAlmostEqual(resultado["prima"], 2000000.0, 2)
+        self.assertAlmostEqual(resultado["vacaciones"], 833333.33, 2)
+        self.assertAlmostEqual(resultado["total"], 5033333.33, 2)
+
+    def test_trabajo_medio_tiempo(self):
+        resultado = liquidacion.calcular_liquidacion_definitiva(1000000, 180, 102854)
+        self.assertAlmostEqual(resultado["cesantias"], 551427.0, 2)
+        self.assertAlmostEqual(resultado["intereses_cesantias"], 6617.12, 2)
+        self.assertAlmostEqual(resultado["prima"], 551427.0, 2)
+        self.assertAlmostEqual(resultado["vacaciones"], 250000.0, 2)
+        self.assertAlmostEqual(resultado["total"], 1355471.12, 2)
+
+    def test_renuncia_voluntaria(self):
+        resultado = liquidacion.calcular_liquidacion_definitiva(1500000, 90, 102854)
+        self.assertAlmostEqual(resultado["cesantias"], 400713.50, 2)
+        self.assertAlmostEqual(resultado["intereses_cesantias"], 12021.41, 2)
+        self.assertAlmostEqual(resultado["prima"], 400713.50, 2)
+        self.assertAlmostEqual(resultado["vacaciones"], 375000.0, 2)
+        self.assertAlmostEqual(resultado["total"], 1188448.41, 2)
+
+    def test_salario_integral(self):
         resultado = liquidacion.calculo_salario_integral()
         self.assertEqual(resultado, "El salario integral no genera prestaciones sociales.")
-def contrato_menor_a_un_mes(self):
+
+    def test_contrato_menor_a_un_mes(self):
         resultado = liquidacion.calcular_liquidacion_definitiva(1200000, 15, 102854)
-        self.assertAlmostEqual(resultado["cesantias"]54285.58, 3)
-        self.assertAlmostEqual(resultado["intereses_cesantias"],271,43, 2)
-        self.assertAlmostEqual(resultado["prima"]54285.58, 3)
-        self.assertAlmostEqual(resultado["vacaciones"]50000)
-        self.assertAlmostEqual(resultado["total"]125842.59, 2)
+        self.assertAlmostEqual(resultado["cesantias"], 54285.58, 2)
+        self.assertAlmostEqual(resultado["intereses_cesantias"], 271.43, 2)
+        self.assertAlmostEqual(resultado["prima"], 54285.58, 2)
+        self.assertAlmostEqual(resultado["vacaciones"], 50000.0, 2)
+        self.assertAlmostEqual(resultado["total"], 158842.59, 2)
+
 if __name__ == '__main__':
     unittest.main()
